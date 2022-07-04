7ae2be31f755ae2846a7e1c123b64cd951be2ecc
Nikhil Dev <nikhil.d@menerva.tech>
2022-07-04 23:48:45 +0530

    added hello world command

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h2>Login</h2>
-       <button>Submit</button>
+        <h2>Login</h2>
+        <button>Submit</button>
         <h2>Logout</h2>
         <button>Submit</button>
       </header>
-      <footer>
-        Footer of my website
-      </footer>
+      <footer>Footer of my website</footer>
       <div>Not required by other branches</div>
     </div>
   );
 }
 
 export default App;
+
+// default
+master;
+
+//custom
+main;
+
+//custom
+development;
+
+//custom
+feature;
